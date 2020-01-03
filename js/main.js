10750a62bc5ea96beb7694fdc5000f1cf37bbf07
Abay <abayibrayev7@gmail.com>
2020-01-03 17:31:50 +0000

    added nav menu functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 $(document).ready(function () {
 /* ---------------------------- slider settings ---------------------------- */
-  $(".reviews-slider").slick({
-    autoplay: true,
-    dots: true,
-    arrows: false,
-    dotsClass: 'reviews-slider__dots'
+    $(".reviews-slider").slick({
+      autoplay: true,
+      dots: true,
+      arrows: false,
+      dotsClass: 'reviews-slider__dots'
+    });
+
+/* -------------------------- anchor links settings ------------------------- */
+  $('a[href^="#"]').click(function () {
+    var _href = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(_href).offset().top - 120 + 'px'
+    });
+    return false;
+  });
+
+/* ---------------------------------- menu ---------------------------------- */
+  let switcher = $('.switcher');
+  $('.nav__link').click(function () {
+    if (switcher.prop('checked') == true) {
+      $('.nav').css('right', '-100%');
+      switcher.prop('checked', false);
+    } 
+  });
+  switcher.click(function() {
+    if (switcher.prop('checked') == true) {
+      $('.nav').css('right', '0%');
+    } else {
+      $('.nav').css('right', '-100%');
+    }
   });
 });
