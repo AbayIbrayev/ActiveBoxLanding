6788be2b09800f5548494867309d0f7bffd2c4f4
Abay <abayibrayev7@gmail.com>
2020-01-03 16:15:57 +0000

    added slick slider to the reviews section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+/* ---------------------------- slider settings ---------------------------- */
+  $(".reviews-slider").slick({
+    autoplay: true,
+    dots: true,
+    arrows: false,
+    dotsClass: 'reviews-slider__dots'
+  });
+});
